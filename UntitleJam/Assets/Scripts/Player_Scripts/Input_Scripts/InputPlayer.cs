2444a658753021852fda6042476cfaa9b01109cb
RaideNnigth <32282947+RaideNnigth@users.noreply.github.com>
2020-09-14 01:01:45 -0300

    movimento

---
+++ UntitleJam/Assets/Scripts/Player_Scripts/Input_Scripts/InputPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementPlayer))]
+public class InputPlayer : MonoBehaviour
+{
+    float axisX;
+    [SerializeField]
+    MovementPlayer movementPlayer;
+
+    void Update()
+    {
+        axisX = Input.GetAxis("Horizontal");
+        Vector2 vector = new Vector2(axisX, movementPlayer.rb.velocity.y);
+
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+
+        Move(vector);
+    }
+
+    void Move(Vector2 vector)
+    {
+        movementPlayer.MovePlayer(vector);
+    }
+    
+    void Jump()
+    {
+        movementPlayer.JumpPlayer();
+    }
+}
